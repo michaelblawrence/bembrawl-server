e83c99c9473ae067f2ca7be4bbab526a84988718
Michael Lawrence <mblawrence27@gmail.com>
2020-04-20 01:49:19 +0100

    cleanup env

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,14 +2,12 @@
 import { Module } from '@nestjs/common';
 
 import { CommonModule } from './common';
-import { PassengerModule } from './passenger/passenger.module';
 import { PlayersModule } from './players/players.module';
 import { HostsModule } from './hosts/hosts.module';
 
 @Module({
     imports: [
         CommonModule,
-        PassengerModule,
         PlayersModule,
         HostsModule
     ]
