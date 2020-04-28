6f88333d1144bac0ff7ffcd8bf2f62078592726f
Michael Lawrence <mblawrence27@gmail.com>
2020-04-28 22:19:13 +0100

    log to file, timestamps, standard logging game, players

--- src/modules/common/provider/logger.service.ts
+++ src/modules/common/provider/logger.service.ts
@@ -1,48 +1,89 @@
-
-import * as winston from 'winston';
+import * as winston from "winston";
+import { PlayersState } from "../model/PlayersState";
+import { GameState } from "../model/GameState";
 
 export class LoggerService {
-
     private readonly instance: winston.Logger;
 
     public constructor() {
-
-        const format = this.isProductionEnv() ?
-            winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.json()
-            ) :
-            winston.format.combine(
-                winston.format.colorize(),
-                winston.format.simple()
-            );
+        const format = this.isProductionEnv()
+            ? winston.format.combine(
+                  winston.format.timestamp(),
+                  winston.format.json()
+              )
+            : winston.format.combine(
+                  winston.format.colorize(),
+                  winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss.SSS'}),
+                  winston.format.printf((info) => `${info.timestamp} ${info.level} ${info.message}`)
+              );
 
         this.instance = winston.createLogger({
-            level: 'info',
+            level: "info",
             silent: this.isTestEnv(),
             format,
             transports: [
                 new winston.transports.Console({
-                    stderrLevels: ['error']
-                })
+                    stderrLevels: ["error"],
+                }),
+                new winston.transports.File({
+                    filename: "bembrawl.info.log",
+                    handleExceptions: true,
+                    zippedArchive: true
+                }),
             ],
         });
     }
 
-    public info(message: string) {
-        this.instance.info(message);
+    public info(
+        message: string,
+        player: PlayersState | null = null,
+        game: GameState | null = null
+    ) {
+        const prefix = this.gameLogInfo(game) + this.playerLogInfo(player);
+        this.instance.info(prefix + message);
     }
 
-    public error(message: string) {
-        this.instance.error(message);
+    public error(
+        message: string,
+        player: PlayersState | null = null,
+        game: GameState | null = null
+    ) {
+        const prefix = this.gameLogInfo(game) + this.playerLogInfo(player);
+        this.instance.error(prefix + message);
     }
 
     private isTestEnv(): boolean {
-        return process.env.NODE_ENV === 'test';
+        return process.env.NODE_ENV === "test";
     }
 
     private isProductionEnv(): boolean {
-        return process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging';
+        return (
+            process.env.NODE_ENV === "production" ||
+            process.env.NODE_ENV === "staging"
+        );
     }
 
+    private playerLogInfo(player: PlayersState | null): string {
+        if (!player) return "";
+        try {
+            const pingedMsAgo = Date.now() - player.getLastKeepAliveMs();
+            return `[(P) ${player.sessionId} idx=${player.getJoinOrder()} ms=${pingedMsAgo}] `;
+        } catch (ex) {
+            this.instance.error("{P-INFO-ERROR}: " + ex);
+            return `[(P) {P-INFO-ERROR}] `;
+        }
+    }
+
+    private gameLogInfo(game: GameState | null): string {
+        if (!game) return "";
+        try {
+            const hostsCount = Object.keys(game.hosts).length;
+            const playersCount = Object.keys(game.players).length;
+            const closedYn = game.closed() ? "Y" : "N";
+            return `[(G) ${game.guid} room=${game.joinId} H=${hostsCount},P=${playersCount},C=${closedYn}] `;
+        } catch (ex) {
+            this.instance.error("{G-INFO-ERROR}: " + ex);
+            return `[(G) {G-INFO-ERROR}] `;
+        }
+    }
 }
